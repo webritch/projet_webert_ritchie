43f13f9325605f3949e67b48b6ec6530ff161e4d
Webert ESTRAME Ing <webertestrameing@yahoo.fr>
2014-03-11 15:51:43 -0700

    my django...

--- webertritchy/urls.py
+++ webertritchy/urls.py
@@ -32,5 +32,5 @@ urlpatterns = patterns('application.views',
 )
 
 urlpatterns = patterns('',
-                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', include(admin.site.urls)),
 )
